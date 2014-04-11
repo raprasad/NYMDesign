8ca4c525ad94a6620ca9875749a7142fffba231f
Raman Prasad <raprasad@gmail.com>
2014-04-11 18:17:46 -0400

    update wsgi file

--- nymdesign/nymdesign/wsgi.py
+++ nymdesign/nymdesign/wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'nymdesign.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nymdesign.settings_prod'
 
 from django.core.handlers.wsgi import WSGIHandler
 
