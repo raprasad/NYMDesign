c339478ef53c2cf7a454e0f374bc90632c3da608
Raman Prasad <raprasad@gmail.com>
2017-08-18 22:59:07 -0400

    updated wsgi

--- nymdesign/wsgi.py
+++ nymdesign/wsgi.py
@@ -3,6 +3,6 @@ import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'nymdesign.settings.webfaction'
 
-from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
 
-application = WSGIHandler()
+application = get_wsgi_application()
